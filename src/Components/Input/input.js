ba79b4afaea9868f2ec8ced5f066ef1562ea08c6
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2023-11-29 23:34:02 +0900

    feat: 인풋 컴포넌트 모달창인 경우 추가

--- src/Components/Input/input.js
+++ src/Components/Input/input.js
@@ -3,15 +3,29 @@ import * as style from './styles';
 
 export default function Input(props) {
   return (
-    <label>
-      <span>{props.content}</span>
-      <style.Input
-        type={props.type}
-        width={props.width}
-        height={props.height}
-        marginBottom={props.marginBottom}
-        marginLeft={props.marginLeft}
-      />
-    </label>
+    <>
+      {props.isModal ? (
+        <div>
+          <style.Input
+            type={props.type}
+            width={props.width}
+            height={props.height}
+            marginBottom={props.marginBottom}
+            marginLeft={props.marginLeft}
+          />
+        </div>
+      ) : (
+        <label>
+          <span>{props.content}</span>
+          <style.Input
+            type={props.type}
+            width={props.width}
+            height={props.height}
+            marginBottom={props.marginBottom}
+            marginLeft={props.marginLeft}
+          />
+        </label>
+      )}
+    </>
   );
 }
