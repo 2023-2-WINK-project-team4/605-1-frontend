10608ad3cc3e094236a2482931931809e45f22db
intelligent04 <87917946+intelligent04@users.noreply.github.com>
2023-12-31 01:40:54 +0900

    fix: 모달창 테두리와 버튼 색깔 일치시킴

--- src/Components/Modal/SeatModal/seatModal.js
+++ src/Components/Modal/SeatModal/seatModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as style from './styles.js';
 import FullBtn from '../../Button/fullBtn';
 import { themeWink, themeFoscar } from '../../Theme/theme';
@@ -9,10 +9,27 @@ export default function SeatModal({
   myReservationInfo,
   club,
 }) {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('click', handleOutsideClick);
+    }
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isOpen, closeModal]);
+
   if (!isOpen) return null;
 
   return (
-    <style.ModalContainer>
+    <style.ModalContainer ref={modalRef}>
       <style.ModalContent theme={club === 'wink' ? themeWink : themeFoscar}>
         <style.ModalCloseButton onClick={closeModal}>X</style.ModalCloseButton>
         <p>{myReservationInfo.seatNumber}번 좌석을 반납하시겠습니까?</p>
