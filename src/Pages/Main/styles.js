37a627972bc122f80f507c843d5217205d052a2f
승준 <robin1415@kookmin.ac.kr>
2024-01-17 18:23:52 +0900

    feat:메인 화면 구현

---
+++ src/Pages/Main/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  img {
+    width: max-content;
+    height: max-content;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  width: 284px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ReservationContainer = styled.div`
+  width: 323px;
+  height: 193px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 99px;
+`;
