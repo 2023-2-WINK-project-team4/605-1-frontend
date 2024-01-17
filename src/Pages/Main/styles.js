66e560e0924d0cf440c782ea0e11c2052cc414b7
승준 <robin1415@kookmin.ac.kr>
2024-01-17 22:13:52 +0900

    feat: 메인 화면 기능 구현 및 디자인 수정

--- src/Pages/Main/styles.js
+++ src/Pages/Main/styles.js
@@ -3,25 +3,33 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   img {
     width: max-content;
     height: max-content;
   }
+  padding: 32px;
+  gap: 100px;
 `;
-
-export const ButtonContainer = styled.div`
-  width: 284px;
-  height: 100%;
+export const BodyContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  gap: 100px;
 `;
 
 export const ReservationContainer = styled.div`
-  width: 323px;
-  height: 193px;
+  width: max-content;
+  height: 100%;
   display: flex;
+  justify-content: center;
   flex-direction: column;
-  justify-content: space-between;
-  margin-bottom: 99px;
+  gap: 40px;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 40px;
 `;
