71188f72dc6092eda9baf7e4428d22eaa63e5bb6
intelligent04 <dlchdaud04@naver.com>
2024-01-19 16:48:45 +0900

    fix: 모달 토글함수를 모달 내부로 넣음

--- src/Components/Modal/SeatModal/seatModal.js
+++ src/Components/Modal/SeatModal/seatModal.js
@@ -1,42 +1,48 @@
 import * as style from './styles';
 import FullBtn from '../../Button/fullBtn';
-import React from 'react';
+import React, { useState } from 'react';
 import { themeWink, themeFoscar } from '../../Theme/theme';
 
-export default function SeatModal({
-  isOpen,
-  closeModal,
-  myReservationInfo,
-  club,
-  tryToSeat,
-}) {
+export default function SeatModal(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
-    isOpen && (
+    isModalOpen && (
       <style.ModalWrapper>
-        <style.ModalContainer onClick={closeModal} />
-        <style.ModalContent theme={club === 'wink' ? themeWink : themeFoscar}>
-          <style.ModalCloseButton onClick={closeModal}>
+        <style.ModalContainer onClick={handleCloseModal} />
+        <style.ModalContent
+          theme={props.club === 'wink' ? themeWink : themeFoscar}
+        >
+          <style.ModalCloseButton onClick={handleCloseModal}>
             X
           </style.ModalCloseButton>
           <div>
-            {myReservationInfo.seatNumber !== 0 ? (
-              <p>{myReservationInfo.seatNumber}번 좌석을 반납하시겠습니까?</p>
+            {props.myReservationInfo.seatNumber !== 0 ? (
+              <p>
+                {props.myReservationInfo.seatNumber}번 좌석을 반납하시겠습니까?
+              </p>
             ) : (
-              <p>{tryToSeat}번 좌석을 배정하시겠습니까?</p>
+              <p>{props.tryToSeat}번 좌석을 배정하시겠습니까?</p>
             )}
           </div>
           <style.ButtonBox>
             <FullBtn
               size="small"
               name="확인"
-              club={club}
-              onClick={closeModal}
+              club={props.club}
+              onClick={handleCloseModal}
             />
             <FullBtn
               size="small"
               name="취소"
-              club={club}
-              onClick={closeModal}
+              club={props.club}
+              onClick={handleCloseModal}
             />
           </style.ButtonBox>
         </style.ModalContent>
--- src/Components/Setting/MyReservation/myReservation.js
+++ src/Components/Setting/MyReservation/myReservation.js
@@ -5,44 +5,31 @@ import StrokeSquareBtn from '../../Button/strokeSquareBtn';
 import axios from 'axios';
 
 export default function MyReservation() {
-  // const MyReservationInfo = {
-  //   seatNumber: 4,
-  //   seatStatus: 'using',
-  //   startTime: '2023-11-14T13:41:23.521Z',
-  // };
-  useEffect(() => {
-    axios
-      .get('seat/my-seat') // 나중에 seat/my-seat 앞에 주소 들어가야됨
-      .then((res) => {
-        const MyReservationInfo = res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
+  const MyReservationInfo = {
+    seatNumber: 4,
+    seatStatus: 'using',
+    startTime: '2023-11-14T13:41:23.521Z',
   };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  // useEffect(() => {
+  //   axios
+  //     .get('seat/my-seat') // 나중에 seat/my-seat 앞에 주소 들어가야됨
+  //     .then((res) => {
+  //       const MyReservationInfo = res;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   if (MyReservationInfo.seatStatus !== 'using') {
     return null;
   } else {
     return (
       <>
-        <SeatModal
-          club="wink"
-          isOpen={isModalOpen}
-          closeModal={handleCloseModal}
-          myReservationInfo={MyReservationInfo}
-        />
+        <SeatModal club="wink" myReservationInfo={MyReservationInfo} />
         <style.myReservationContainer>
           <style.title>나의 예약</style.title>
-          <div className="return box" onClick={handleOpenModal}>
+          <div className="return box" onClick={SeatModal.handleOpenModal}>
             <style.myReservationWrapper>
               <StrokeSquareBtn
                 size="middle"
