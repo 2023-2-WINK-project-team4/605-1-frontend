ce4fb7c871cdfefdaa17a01783ef351614121b88
김민선 <alstjs4739@kookmin.ac.kr>
2024-02-07 15:54:08 +0900

    feat: 로그인 페이지 백엔드 연결

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ProfileEdit from './Pages/ProfileEdit/profileEdit';
 import Setting from './Pages/Setting/setting';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginCallBack from './Pages/Login/loginCallback';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/loginCallback" element={<LoginCallBack />} />
           <Route path="/setting" element={<Setting />} />
           <Route path="/profileEdit" element={<ProfileEdit />} />
         </Routes>
