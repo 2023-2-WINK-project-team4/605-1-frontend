e6c0361bdfc2cfd99221f6547a5f8e6f6497e079
intelligent04 <dlchdaud04@naver.com>
2024-03-07 21:51:19 +0900

    fix: myReservation 코드 중복 수정

--- src/Components/Setting/MyReservation/myReservation.js
+++ src/Components/Setting/MyReservation/myReservation.js
@@ -1,72 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import * as style from './styles';
+import SeatModal from '../../Modal/SeatModal/seatModal';
+import StrokeSquareBtn from '../../Button/strokeSquareBtn';
 import axios from 'axios';
-import FullBtn from '../../Button/fullBtn';
-import { useNavigate } from 'react-router-dom';
 
-export default function MyProfile() {
-  const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState({});
+export default function MyReservation() {
   const url = process.env.REACT_APP_API_URL;
+  const [myReservationInfo, setMyReservationInfo] = useState({
+    seatNumber: 7,
+    startTime: '2023-11-14T13:41:23.521Z',
+  });
 
   useEffect(() => {
-    const jwtToken = sessionStorage.getItem('token'); // 로컬 스토리지에서 토큰을 가져옵니다.
-
-    // 토큰이 없으면 로그인 페이지로 이동
-    if (!jwtToken) {
-      navigate('/login');
-      return;
-    }
-
-    // 토큰이 있으면 정상적으로 진행
     axios
-      .get(url + 'user', {
-        headers: {
-          Authorization: `Bearer ${jwtToken}`,
-        },
-      })
+      .get(url + 'seat/my-seat')
       .then((res) => {
-        setUserInfo(res.data);
+        setMyReservationInfo(res.data);
       })
       .catch((error) => {
         console.log(error);
-        // 토큰이 유효하지 않거나 기타 오류가 발생하면 로그인 페이지로 이동.
-        navigate('/login');
       });
-  }, [navigate, url]);
+  }, []);
 
-  return (
-    <>
-      <style.title>나의 프로필</style.title>
-      <style.userInfoContainer>
-        <style.userInfoWrapper>
-          <style.userProfile>
-            <img src={userInfo.profile} alt="프로필 사진" />
-          </style.userProfile>
-          <style.userInfoBox>
-            <style.userInfo>
-              <span>이름</span>
-              <span>{userInfo.name}</span>
-            </style.userInfo>
-            <style.userInfo>
-              <span>소속</span>
-              <span>{userInfo.club}</span>
-            </style.userInfo>
-            <style.userInfo>
-              <span>학번</span>
-              <span>{userInfo.studentID}</span>
-            </style.userInfo>
-          </style.userInfoBox>
-        </style.userInfoWrapper>
-        <FullBtn
-          size="big"
-          club={userInfo.club}
-          name="프로필 수정하기"
-          onClick={() => {
-            navigate('/profileEdit', { state: { userInfo } });
-          }}
-        />
-      </style.userInfoContainer>
-    </>
-  );
+  const returnSeat = () => {
+    axios
+      .patch(url + 'seat/return')
+      .then((res) => {
+        console.log('좌석반납완료');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (!myReservationInfo.seatNumber) {
+    return null;
+  } else {
+    return (
+      <>
+        <style.myReservationContainer>
+          <style.title>나의 예약</style.title>
+          <SeatModal
+            children={
+              <div className="return box">
+                <style.myReservationWrapper>
+                  <StrokeSquareBtn
+                    size="middle"
+                    club="wink"
+                    onClick={returnSeat}
+                    url={
+                      process.env.PUBLIC_URL + '/Images/All/winkSeatIcon.svg'
+                    }
+                  />
+                  <style.seatInfo>
+                    {myReservationInfo.seatNumber}번 좌석 사용중
+                  </style.seatInfo>
+                </style.myReservationWrapper>
+              </div>
+            }
+            club="wink"
+            myReservationInfo={myReservationInfo}
+          />
+        </style.myReservationContainer>
+      </>
+    );
+  }
 }
