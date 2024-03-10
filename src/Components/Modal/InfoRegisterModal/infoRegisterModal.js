cb69156aff8ce07b988e5e47c379615c9b9831f2
김민선 <alstjs4739@kookmin.ac.kr>
2024-03-10 19:01:44 +0900

    style: infoRegisterModal 컴포넌트 코드 구조 수정

--- src/Components/Modal/InfoRegisterModal/infoRegisterModal.js
+++ src/Components/Modal/InfoRegisterModal/infoRegisterModal.js
@@ -1,196 +1,129 @@
 import React, { useState } from 'react';
-import { themeWink, themeFoscar } from '../../Theme/theme';
-import Button from '../../Button/fullBtn';
+import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+import FullBtn from '../../Button/fullBtn';
+import StrokeBtn from '../../Button/strokeBtn';
 import Dropdown from '../../Dropdown/dropdown';
-import DatePicker from 'react-mobile-datepicker';
+import Input from '../../Input/input';
 import * as style from './styles';
 
 export default function InfoRegisterModal(props) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const [name, setName] = useState('');
   const [seatNum, setSeatNum] = useState('');
   const [reason, setReason] = useState('');
   const [club, setClub] = useState('');
   const [reps, setReps] = useState('');
-  const [time, setTime] = useState(new Date());
 
-  const handleDateSelect = (time) => {
-    handleDateCancel();
+  const handleCloseBtn = () => {
+    setIsOpen(false);
   };
 
-  const handleDateCancel = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const dateConfig = {
-    year: {
-      format: 'YYYY',
-      caption: '년',
-      step: 1,
-    },
-    month: {
-      format: 'MM',
-      caption: '월',
-      step: 1,
-    },
-    date: {
-      format: 'DD',
-      caption: '일',
-      step: 1,
-    },
-    hour: {
-      format: 'hh',
-      caption: '시',
-      step: 1,
-    },
-    minute: {
-      format: 'mm',
-      caption: '분',
-      step: 1,
-    },
-    meridiem: {
-      format: (value) => (value.hour < 12 ? '오전' : '오후'),
-      caption: '오전/오후',
-      step: 1,
-    },
-  };
-
-  const handleClose = () => {
-    props.closeModal();
-  };
-
-  const handleConfirm = () => {
-    // 정보 저장
-    props.closeModal();
+  const handleConfirmBtn = () => {
+    setIsOpen(false);
   };
 
   return (
     <>
-      {props.openModal && (
-        <style.ModalContainer>
-          <style.ModalContent
-            theme={props.club === 'wink' ? themeWink : themeFoscar}
-            pageName={props.pageName}
-          >
-            <style.ModalHeader>
-              <span>
-                {props.pageName === 'meetingTable'
-                  ? '부정 사용자 신고하기'
-                  : '예약하기'}
-              </span>
-              <style.CloseButton onClick={handleClose}>X</style.CloseButton>
-            </style.ModalHeader>
-
-            <style.FormContainer>
-              <style.InputContainer>
-                <Button
-                  size="small"
-                  name={
-                    props.pageName === 'meetingTable' ? '이름' : '사용 동아리'
-                  }
-                  club={props.club}
-                />
-                {props.pageName === 'meetingTable' ? (
-                  <style.Input
-                    type={'text'}
-                    width={'148px'}
-                    height={'20px'}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                ) : (
-                  <Dropdown
-                    club={'WINK'}
-                    width={'156px'}
-                    height={'34px'}
-                    onChange={(e) => setClub(e.target.value)}
-                  />
-                )}
-              </style.InputContainer>
-              <style.InputContainer>
-                <Button
-                  size="small"
-                  name={
-                    props.pageName === 'meetingTable' ? '좌석 번호' : '대표자'
-                  }
-                  club={props.club}
-                />
-                {props.pageName === 'meetingTable' ? (
-                  <style.Input
-                    type={'number'}
-                    width={'116px'}
-                    height={'20px'}
-                    onChange={(e) => setSeatNum(e.target.value)}
-                  />
-                ) : (
-                  <style.Input
-                    type={'text'}
-                    width={'168px'}
-                    height={'20px'}
-                    onChange={(e) => setReps(e.target.value)}
-                  />
-                )}
-              </style.InputContainer>
-              <style.InputContainer
-                column
-                style={
-                  props.pageName === 'meetingTable'
-                    ? {}
-                    : { marginLeft: '-204px' }
-                }
-              >
-                <Button
-                  size="small"
-                  name={
-                    props.pageName === 'meetingTable'
-                      ? '신고 사유'
-                      : '사용 시간'
-                  }
-                  club={props.club}
-                />
-
-                {props.pageName === 'meetingTable' ? (
-                  <style.Input
-                    as="textarea"
-                    type={'textarea'}
-                    width={'224px'}
-                    height={'88px'}
-                    onChange={(e) => setReason(e.target.value)}
-                  />
-                ) : (
-                  <style.DatePickerContainer>
-                    <DatePicker
-                      value={time}
-                      isPopup={false}
-                      isOpen={true}
-                      dateConfig={dateConfig}
-                      showHeader={false}
-                      showFooter={false}
-                      onSelect={handleDateSelect}
-                      onCancel={handleDateCancel}
-                    />
-                  </style.DatePickerContainer>
-                )}
-              </style.InputContainer>
-            </style.FormContainer>
-            <style.ButtonContainer pageName={props.pageName}>
-              <div>
-                <Button
-                  size="small"
-                  name="확인"
-                  club={props.club}
-                  onClick={handleConfirm}
-                />
-                <Button
-                  size="small"
-                  name="취소"
-                  club={props.club}
-                  onClick={handleClose}
-                />
-              </div>
-            </style.ButtonContainer>
-          </style.ModalContent>
-        </style.ModalContainer>
-      )}
+      <Modal
+        pageName={props.pageName}
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        style={style.customModalStyles}
+        ariaHideApp={false}
+        contentLabel="infomation & register modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <style.ModalHeader>
+          {props.pageName === 'meetingTable'
+            ? '예약하기'
+            : '부정 사용자 신고하기'}
+          <style.CloseButton
+            src={process.env.PUBLIC_URL + '/Images/All/closeIcon.svg'}
+            onClick={handleCloseBtn}
+          />
+        </style.ModalHeader>
+        <style.ModalContent>
+          <style.ModalItem>
+            <FullBtn
+              size="small"
+              name={props.pageName === 'meetingTable' ? '사용 동아리' : '이름'}
+              club={props.club}
+            />
+            {props.pageName === 'meetingTable' ? (
+              <Dropdown
+                club={'wink'}
+                width={'160px'}
+                height={'34px'}
+                onChange={(e) => setClub(e.target.value)}
+              />
+            ) : (
+              <Input
+                isModal
+                type={'text'}
+                width={'180px'}
+                height={'20px'}
+                onChange={(e) => setName(e.target.value)}
+              />
+            )}
+          </style.ModalItem>
+          <style.ModalItem>
+            <FullBtn
+              size="small"
+              name={props.pageName === 'meetingTable' ? '대표자' : '좌석 번호'}
+              club={props.club}
+            />
+            {props.pageName === 'meetingTable' ? (
+              <Input
+                isModal
+                type={'text'}
+                width={'168px'}
+                height={'20px'}
+                onChange={(e) => setReps(e.target.value)}
+              />
+            ) : (
+              <Input
+                isModal
+                type={'number'}
+                width={'152px'}
+                height={'20px'}
+                onChange={(e) => setSeatNum(e.target.value)}
+              />
+            )}
+          </style.ModalItem>
+          <style.ModalItem>
+            <FullBtn
+              size="small"
+              name={
+                props.pageName === 'meetingTable' ? '사용 시간' : '신고 사유'
+              }
+              club={props.club}
+            />
+            {props.pageName === 'meetingTable' ? (
+              <style.DatePickerContainer>
+                <DatePicker />
+              </style.DatePickerContainer>
+            ) : (
+              <Input
+                isModal
+                isArea
+                width={'240px'}
+                height={'88px'}
+                onChange={(e) => setReason(e.target.value)}
+              />
+            )}
+          </style.ModalItem>
+        </style.ModalContent>
+        <style.ModalFooter pageName={props.pageName}>
+          <FullBtn
+            size="small"
+            name="확인"
+            club={props.club}
+            onClick={handleConfirmBtn}
+          />
+          <StrokeBtn name="취소" club={props.club} onClick={handleCloseBtn} />
+        </style.ModalFooter>
+      </Modal>
     </>
   );
 }
--- src/Components/Modal/InfoRegisterModal/styles.js
+++ src/Components/Modal/InfoRegisterModal/styles.js
@@ -1,95 +1,67 @@
 import styled from 'styled-components';
 
-export const ModalContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.3);
-`;
-
-export const ModalContent = styled.div`
-  width: ${(props) => (props.pageName === 'meetingTable' ? '260px' : '280px')};
-  height: ${(props) => (props.pageName === 'meetingTable' ? '356px' : '460px')};
-  background: white;
-  border: 2px solid ${(props) => props.theme.color};
-  border-radius: 10px;
-  padding: 20px;
-`;
+export const customModalStyles = {
+  overlay: {
+    backgroundColor: ' rgba(0, 0, 0, 0.4)',
+    width: '100%',
+    height: '100vh',
+    zIndex: '10',
+    position: 'fixed',
+    top: '0',
+    left: '0',
+  },
+  content: {
+    width: '260px',
+    height: '360px',
+    zIndex: '150',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '10px',
+    boxShadow: '2px 2px 2px #00000040',
+    backgroundColor: 'white',
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '20px',
+    padding: '24px 12px 0px 12px',
+    border: '2px solid #3a70ff',
+  },
+};
 
 export const ModalHeader = styled.div`
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  font-weight: 600;
-  margin: 8px 0 20px 4px;
   font-size: 16px;
-  font-weight: 600;
-  color: black;
-  background: none;
-  border: none;
+  font-weight: 700;
+`;
+
+export const CloseButton = styled.img`
   cursor: pointer;
 `;
 
-export const CloseButton = styled.button`
-  font-size: 16px;
-  font-weight: 600;
-  color: black;
-  background: none;
-  border: none;
-  cursor: pointer;
-`;
-
-export const FormContainer = styled.div`
+export const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 12px;
+  gap: 14px;
 `;
 
-export const InputContainer = styled.div`
+export const ModalItem = styled.div`
   display: flex;
-  flex-direction: ${(props) => (props.column ? 'column' : 'row')};
-  align-items: flex-start;
-  gap: ${(props) => (props.column ? '12px' : '32px')};
-  vertical-align: ${(props) => (props.column ? 'top' : 'middle')};
+  justify-content: space-between;
+  gap: 12px;
+  &:last-child {
+    flex-direction: column;
+  }
 `;
 
-export const Input = styled.input`
-  width: ${(props) => (props.column ? '228px' : props.width)};
-  height: ${(props) => props.height};
-  border: 1px solid black;
-  border-radius: 8px;
-  padding: 7px 10px;
-  text-align: ${(props) => (props.centerText ? 'center' : 'left')};
-  ${(props) =>
-    props.type === 'textarea'
-      ? `
-          white-space: pre-wrap;
-          overflow: auto;
-          resize: none;
-        `
-      : ''}
+export const ModalFooter = styled.div`
+  display: flex;
+  align-self: flex-end;
+  gap: 20px;
 `;
 
 export const DatePickerContainer = styled.div`
-  position: absolute;
-  width: 284px;
-  top: 80%;
-`;
-
-export const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  margin-top: ${(props) =>
-    props.pageName === 'meetingTable' ? '20px' : '240px'};
-  button:first-child {
-    margin-right: 20px;
-  }
+  width: 230px;
 `;
