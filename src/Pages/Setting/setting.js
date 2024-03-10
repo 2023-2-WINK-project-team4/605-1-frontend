1856e0d4ce0bb3488e030fe6d334973f1abaeaa5
김민선 <alstjs4739@kookmin.ac.kr>
2024-03-10 15:06:42 +0900

    feat: 설정 화면 백엔드 연결

--- src/Pages/Setting/setting.js
+++ src/Pages/Setting/setting.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as style from './styles';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header/header';
 import Footer from '../../Components/Footer/footer';
 import Box from '../../Components/Box/box';
 import FullBtn from '../../Components/Button/fullBtn';
+import axios from 'axios';
 
 export default function Setting() {
   const title = '설정';
-  const navigate = useNavigate();
 
-  const userInfo = {
-    name: '유건',
-    studentId: 20203103,
-    club: 'wink',
-    profile:
-      'https://t3.ftcdn.net/jpg/04/73/67/64/240_F_473676400_VyH1ey15WGBA6L9MILjha6thtMVfuRh2.jpg',
-    kakaoId: '카카오아이디',
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState({});
+  const [seatInfo, setSeatInfo] = useState('');
+
+  const token = sessionStorage.getItem('token');
+
+  const getUser = async () => {
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/user/update`, {
+        headers: { Authorization: `${token}` },
+      })
+      .then((res) => {
+        setUserInfo(res.data.member);
+        setSeatInfo(res.data.seatNumber);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const seatInfo = { seatNumber: '4' };
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <>
@@ -29,7 +42,14 @@ export default function Setting() {
         <style.UserInfoContainer>
           <style.UserInfoWrapper>
             <style.UserProfile>
-              <img src={userInfo.profile} alt="프로필 사진" />
+              {userInfo.profile ? (
+                <img src={userInfo.profile} alt="프로필 사진" />
+              ) : (
+                <img
+                  src={process.env.PUBLIC_URL + '/Images/Setting/profile.svg'}
+                  alt="프로필 사진"
+                />
+              )}
             </style.UserProfile>
             <style.UserInfoBox>
               <style.UserInfo>
@@ -64,8 +84,8 @@ export default function Setting() {
             pageName={'main'}
             src={process.env.PUBLIC_URL + '/Images/All/winkSeatIcon.svg'}
             content={
-              seatInfo.seatNumber != null
-                ? seatInfo.seatNumber + '번 좌석 사용 중'
+              seatInfo != null
+                ? seatInfo + '번 좌석 사용 중'
                 : '이용 중인 좌석이 없습니다.'
             }
           />
